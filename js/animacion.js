7eb643b596f78c708dcaa62eba30db72f4f8e81d
carant123 <carant896@hotmail.com>
2016-02-06 16:54:33 -0500

    comentario nuevo reel

--- js/animacion.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-    $("#modelos").click(function(){
-        $("img3D").hide();
-    });
-    $("#modelos2").click(function(){
-        $("img3D").show();
-    });
-});
